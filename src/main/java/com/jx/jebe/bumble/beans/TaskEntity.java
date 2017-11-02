c7711bb301e9be4d2514332322c27adcf94fad3d
liufeitian <1984hou@163.com>
2017-11-02 10:21:04 +0800

    任务调度初步

--- src/main/java/com/jx/jebe/bumble/beans/TaskEntity.java
+++ src/main/java/com/jx/jebe/bumble/beans/TaskEntity.java
@@ -7,5 +7,37 @@ public class TaskEntity {
     private String runtype;//
     private int SimpleTriggerexpression;
     private String CronTriggerexpression;
+    private Class clz;
 
+    public Class getClz() {
+        return clz;
+    }
+
+    public void setClz(Class clz) {
+        this.clz = clz;
+    }
+
+    public String getRuntype() {
+        return runtype;
+    }
+
+    public void setRuntype(String runtype) {
+        this.runtype = runtype;
+    }
+
+    public int getSimpleTriggerexpression() {
+        return SimpleTriggerexpression;
+    }
+
+    public void setSimpleTriggerexpression(int simpleTriggerexpression) {
+        SimpleTriggerexpression = simpleTriggerexpression;
+    }
+
+    public String getCronTriggerexpression() {
+        return CronTriggerexpression;
+    }
+
+    public void setCronTriggerexpression(String cronTriggerexpression) {
+        CronTriggerexpression = cronTriggerexpression;
+    }
 }
--- src/main/java/com/jx/jebe/bumble/main/GSTaskMain.java
+++ src/main/java/com/jx/jebe/bumble/main/GSTaskMain.java
@@ -1,22 +1,45 @@
 package com.jx.jebe.bumble.main;
 
+import com.jx.jebe.bumble.beans.TaskEntity;
+import com.jx.jebe.bumble.task.AbstractTask;
 import com.jx.spat.gaea.server.bootstrap.Main;
 
-import java.util.Properties;
+import java.util.*;
 
 /**
  * Created by xiaowei on 17/11/1.
  */
 public class GSTaskMain extends Main{
+    private static List<TaskEntity> list = new ArrayList<TaskEntity>();
     static {
         initTaskConfig();
     }
 
     private static void initTaskConfig() {
-
+        
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args)throws Exception{
+        if(null != list && list.size() > 0){
+            for(final TaskEntity te : list){
+                new Timer().schedule(new TimerTask(){
+                    public void run() {
+                        try {
+                            AbstractTask tt = (AbstractTask) te.getClz().newInstance();
+                            try {
+                                tt.runtask();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        } catch (InstantiationException e) {
+                            e.printStackTrace();
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },te.getSimpleTriggerexpression(),te.getSimpleTriggerexpression());
+            }
+        }
         try {
             Main.main(args);
         } catch (Exception e) {
